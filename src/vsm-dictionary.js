9223f7dbae60ddf496b59044a9a6c45a59ddf077
Steven Cruy <stcruy@users.noreply.github.com>
2018-02-28 11:45:56 +0100

    prepare for splitting the package

--- src/vsm-dictionary.js
+++ src/vsm-dictionary.js
@@ -3,4 +3,6 @@ const Dictionary = require('./Dictionary/Dictionary');
 const DictionaryLocal = require('./Dictionary/DictionaryLocal');
 const DictionaryRemoteDemo = require('./Dictionary/DictionaryRemoteDemo');
 
-module.exports = { Dictionary, DictionaryLocal, DictionaryRemoteDemo };
+module.exports = Dictionary;
+module.exports.DictionaryLocal = DictionaryLocal;
+module.exports.DictionaryRemoteDemo = DictionaryRemoteDemo;
